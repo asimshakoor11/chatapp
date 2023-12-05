17b4930fff58b0640485a8a5476ad6075332f440
Muhammad Asim Shakoor <asimshakoor920830@gmail.com>
2023-12-06 00:28:29 +0500

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,6 +1,6 @@
-export const host = "https://chatapp-server-rust.vercel.app/";
-export const loginRoute = `${host}api/auth/login`;
-export const registerRoute = `${host}api/auth/register`;
+export const host = "https://chatapp-server-rust.vercel.app";
+export const loginRoute = `${host}/api/auth/login`;
+export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
 export const allUsersRoute = `${host}/api/auth/allusers`;
 export const sendMessageRoute = `${host}/api/messages/addmsg`;
